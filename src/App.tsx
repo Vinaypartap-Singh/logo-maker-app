acd5e327fd983fc04fcd7d86e1dc608eb92ac0de
Vinaypartap Singh <developervsandhu@gmail.com>
2024-05-11 10:37:49 -0400

    Logo Maker Application

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/customComponent/Header";
+import SideNav from "./components/customComponent/SideNav";
+import IconController from "./components/customComponent/IconController";
+import BackgroundController from "./components/customComponent/BackgroundController";
+import LogoPreview from "./components/customComponent/LogoPreview";
+import { UpdateStorageContext } from "./context/UpdateStorageContext";
+
+function App() {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [updateStorage, setUpdateStorage] = useState({});
+  const [downloadIcon, setDownloadIcon] = useState();
+  return (
+    <UpdateStorageContext.Provider value={{ updateStorage, setUpdateStorage }}>
+      <div>
+        <Header DownloadIcon={setDownloadIcon} />
+        <div className="w-64 fixed h-full border">
+          <SideNav selectedIndex={(value: any) => setSelectedIndex(value)} />
+        </div>
+        <div className="ml-64 grid grid-cols-1 md:grid-cols-6 min-h-screen">
+          <div className="md:col-span-2">
+            {selectedIndex === 0 ? (
+              <IconController />
+            ) : (
+              <BackgroundController />
+            )}
+          </div>
+          <div className="md:col-span-3 bg-blue-100">
+            <LogoPreview downloadIcon={downloadIcon} />
+          </div>
+          <div className="bg-green-100">Ads Banner</div>
+        </div>
+      </div>
+    </UpdateStorageContext.Provider>
+  );
+}
+
+export default App;
